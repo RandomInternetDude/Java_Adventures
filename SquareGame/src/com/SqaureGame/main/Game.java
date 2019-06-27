9b4c29c89043b5000bb4864435128583323d63a2
Joey Rivera <jrivera@trellance.com>
2019-06-27 08:15:08 -0400

    Window opens

--- SquareGame/src/com/SqaureGame/main/Game.java
+++ SquareGame/src/com/SqaureGame/main/Game.java
@@ -4,25 +4,30 @@ import java.awt.Canvas;
 
 public class Game extends Canvas implements Runnable {
 	
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1550691097823471818l;
+	
+	private static final long serialVersionUID = 6430417874509459483L;
 
-
+	public static final int WIDTH = 1640, HEIGHT = WIDTH / 12 * 9;
+	
+	public Game(){
+		new Window(WIDTH, HEIGHT, "PatternDoom", this);
+	}
+	
+	
 	public synchronized void start(){
 		
 	}
 	
+	public void run() {
+		
+		
+	}
 	
 	public static void main(String[] arg) {
-		
+		new Game();
 	}
 
 
-	@Override
-	public void run() {
-		// TODO Auto-generated method stub
-		
-	}
+	
+	
 }
--- SquareGame/src/com/SqaureGame/main/Window.java
+++ SquareGame/src/com/SqaureGame/main/Window.java
@@ -1,16 +1,18 @@
 package com.SqaureGame.main;
 
+
 import java.awt.Canvas;
-import java.awt.Component;
 import java.awt.Dimension;
 
 import javax.swing.JFrame;
 
 public class Window extends Canvas {
 	
-	private static final long serialVersionUID = -240840600533728354L;
 	
-	public Window(int width, int height, String title, Component game) {
+
+	private static final long serialVersionUID = 262501489467400910L;
+
+	public Window(int width, int height, String title, Game game) {
 		JFrame frame = new JFrame(title);
 		frame.setPreferredSize(new Dimension(width,height));
 		frame.setMaximumSize(new Dimension(width,height));
